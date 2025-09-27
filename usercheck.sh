7a87ac5100844feed7a626d6c7ba19ee3986f3ca
rnfor-pro <67124388+rnfor-pro@users.noreply.github.com>
2025-09-27 09:49:36 -0400

    user check script added (#5)

---
+++ usercheck.sh
@@ -0,0 +1,21 @@
+#this script will be used to check if a user exist in a linux server and
+#Also check if the user directory is present or not
+#Before we test this script let's make sure we have already created the script
+#To add user
+
+#!/bin/bash
+
+echo -n " Please enter the username to be checked: "
+read username
+echo " Search Engine Activated!!..searching.. "
+
+if grep $username /etc/passwd; then
+echo " The user account for $username exist in the system.. "
+echo " The user $username is part of Etech ConsultingDevops course "
+elif ls -d /home/$username/; then
+echo " The directory for $username exist in the system "
+echo " Even though the $username acount does not exist "
+else
+echo " The user directory does not exist in the system "
+echo " The user account for $username does not exist "
+fi
